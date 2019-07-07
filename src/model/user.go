bcb675dec34b0e8939c9fcf25392b61db7bbf0f2
Yakhya Dabo <yakhyadabo@gmail.com>
2019-07-07 14:33:33 +0100

    [model] Expose User's properties (id, email)

--- src/model/user.go
+++ src/model/user.go
@@ -1,21 +1,26 @@
 package model
 
+import(
+// "github.com/jinzhu/gorm"
+)
+
 type User struct {
-	id    string
-	email string
+	// gorm.Model
+	Id    string `json:"id"`
+	Email string `json:"email"`
 }
 
 func NewUser(id, email string) *User {
 	return &User{
-		id:    id,
-		email: email,
+		Id:    id,
+		Email: email,
 	}
 }
 
 func (u *User) GetID() string {
-	return u.id
+	return u.Id
 }
 
 func (u *User) GetEmail() string {
-	return u.email
+	return u.Email
 }
