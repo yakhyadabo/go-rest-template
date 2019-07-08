fbb3bf8fc1d2b55defdb7e1c83722e6484a53d17
Yakhya Dabo <yakhyadabo@gmail.com>
2019-07-08 09:01:49 +0100

    [model] New fields for user

--- src/model/user.go
+++ src/model/user.go
@@ -7,18 +7,34 @@ import(
 type User struct {
 	// gorm.Model
 	Id    string `json:"id"`
+	Login string `json:"login"`
+	Password string `json:"password"`
 	Email string `json:"email"`
 }
 
-func NewUser(id, email string) *User {
+func NewUser(id string, login, password, email string) *User {
 	return &User{
 		Id:    id,
+		Login: login,
+		Password: password,
 		Email: email,
 	}
 }
 
 func (u *User) GetID() string {
 	return u.Id
+}
+
+func (u *User) SetID(id string) {
+	u.Id = id
+}
+
+func (u *User) GetLogin() string {
+	return u.Login
+}
+
+func (u *User) GetPassword() string {
+	return u.Password
 }
 
 func (u *User) GetEmail() string {
