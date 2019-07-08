8095fb86cf6ac576d98edc35dc089eebb25d3805
Yakhya Dabo <yakhyadabo@gmail.com>
2019-07-08 09:04:45 +0100

    [controller] Take new fields into account

--- src/controller/userController.go
+++ src/controller/userController.go
@@ -1,18 +1,19 @@
 package controller
 
 import (
-	//"github.com/yakhyadabo/go-rest-template/src/service"
+	// "github.com/yakhyadabo/go-rest-template/src/service"
 	"net/http"
 	"github.com/yakhyadabo/go-rest-template/src/model"
-	//"github.com/yakhyadabo/go-rest-template/src/repository"
+	"github.com/yakhyadabo/go-rest-template/src/repository"
 	"encoding/json"
 	u "github.com/yakhyadabo/go-rest-template/src/utils"
+	"log"
+	"github.com/google/uuid"
+	// "github.com/gorilla/mux"
 )
 
 var CreateUser = func(w http.ResponseWriter, r *http.Request) {
 
-	//userId := r.Context().Value("user") . (uint) //Grab the id of the user that send the request
-	// user := model.NewUser("userId","email")
 	user := &model.User{}
 	err := json.NewDecoder(r.Body).Decode(user)
 	if err != nil {
@@ -20,29 +21,38 @@ var CreateUser = func(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
- // repo := repository.NewUserRepository()
-  // service := service.NewUserService(repo)
+	uid, err := uuid.NewRandom()
+	if err != nil {
+		log.Println("error : ", err)
+		return
+	}
 
-	//user.id = userId
-	// resp :=  repo.Save(user)
+	user.SetID(uid.String())
+
+	repo := repository.NewUserRepository()
+	// service := service.NewUserService(repo)
+
+	repo.Save(user)
 	resp := u.Message(true, "success")
 	resp["user"] = user
 	u.Respond(w, resp)
 }
 
-/*
-var GetContactsFor = func(w http.ResponseWriter, r *http.Request) {
+var GetUser = func(w http.ResponseWriter, r *http.Request) {
+	v := r.URL.Query()
+	login := v.Get("login")
 
-	params := mux.Vars(r)
-	id, err := strconv.Atoi(params["id"])
-	if err != nil {
-		//The passed path parameter is not an integer
-		u.Respond(w, u.Message(false, "There was an error in your request"))
+	log.Println("login : ", login)
+	repo := repository.NewUserRepository()
+	user, error := repo.FindByLogin(string(login))
+
+	if error != nil {
+		u.Respond(w, u.Message(false, "There was an error in your repository"))
 		return
 	}
 
-	data := model.GetContacts(uint(id))
+	// data := model.GetContacts(uint(id))
 	resp := u.Message(true, "success")
-	resp["data"] = data
+	resp["user"] = user
 	u.Respond(w, resp)
-}*/
+}
