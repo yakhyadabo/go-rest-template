a2b47e5234a68135e07d9dd0aa6b3775cd2c535f
Yakhya Dabo <yakhyadabo@gmail.com>
2019-07-09 07:40:05 +0100

    [model] Enable ORM

--- src/model/user.go
+++ src/model/user.go
@@ -1,33 +1,24 @@
 package model
 
 import(
-// "github.com/jinzhu/gorm"
+	"github.com/jinzhu/gorm"
 )
 
 type User struct {
-	// gorm.Model
-	Id    string `json:"id"`
+	gorm.Model
 	Login string `json:"login"`
 	Password string `json:"password"`
 	Email string `json:"email"`
 }
 
-func NewUser(id string, login, password, email string) *User {
+func NewUser(login, password, email string) *User {
 	return &User{
-		Id:    id,
 		Login: login,
 		Password: password,
 		Email: email,
 	}
 }
 
-func (u *User) GetID() string {
-	return u.Id
-}
-
-func (u *User) SetID(id string) {
-	u.Id = id
-}
 
 func (u *User) GetLogin() string {
 	return u.Login
