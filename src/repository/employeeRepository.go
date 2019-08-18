a00e059cb9b93f3b9ce962c8a6d8687d32b30d9c
Yakhya Dabo <yakhyadabo@gmail.com>
2019-08-18 22:30:46 +0100

    [repository] employeeRepository

---
+++ src/repository/employeeRepository.go
@@ -0,0 +1,37 @@
+package repository
+
+import (
+	"github.com/yakhyadabo/go-rest-template/src/model"
+)
+
+type EmployeeRepository interface {
+	FindAll() ([]*model.Employee, error)
+	FindByEmail(email string) (*model.Employee, error)
+	Save(*model.Employee) error
+}
+
+type employeeRepository struct {
+}
+
+func NewEmployeeRepository() *employeeRepository {
+	return &employeeRepository{}
+}
+
+func (r *employeeRepository) Save(employee *model.Employee) (*model.Employee, error) {
+
+	GetDB().Create(employee)
+	GetDB().Save(employee)
+
+	return employee, nil
+}
+
+func (r *employeeRepository) FindByLogin(login string) (*model.Employee, error) {
+
+	employee := &model.Employee{}
+	err := GetDB().Table("employees").Where("login = ?", login).First(employee).Error
+	if err != nil {
+		return nil, nil
+	}
+	return employee, nil
+
+}
