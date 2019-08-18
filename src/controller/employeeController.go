285c03046c935dd8fa104227a5959b5ada048f98
Yakhya Dabo <yakhyadabo@gmail.com>
2019-08-18 22:29:44 +0100

    [controller] employeeController

---
+++ src/controller/employeeController.go
@@ -0,0 +1,53 @@
+package controller
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+
+	"github.com/yakhyadabo/go-rest-template/src/model"
+	"github.com/yakhyadabo/go-rest-template/src/repository"
+	u "github.com/yakhyadabo/go-rest-template/src/utils"
+)
+
+var CreateEmployee = func(w http.ResponseWriter, r *http.Request) {
+
+	employee := &model.Employee{}
+	err := json.NewDecoder(r.Body).Decode(employee)
+	if err != nil {
+		u.Respond(w, u.Message(false, "Error while decoding request body"))
+		return
+	}
+
+	repo := repository.NewEmployeeRepository()
+	// service := service.NewEmployeeService(repo)
+
+	repo.Save(employee)
+	resp := u.Message(true, "success")
+	resp["employee"] = employee
+	u.Respond(w, resp)
+}
+
+var GetEmployee = func(w http.ResponseWriter, r *http.Request) {
+	v := r.URL.Query()
+	login := v.Get("login")
+
+	log.Println("login : ", login)
+	repo := repository.NewEmployeeRepository()
+	employee, error := repo.FindByLogin(string(login))
+
+	if error != nil {
+		u.Respond(w, u.Message(false, "There was an error in your repository"))
+		return
+	}
+
+	if employee == nil {
+		u.Respond(w, u.Message(false, "No employee with login "+login+" found"))
+		return
+	}
+
+	// data := model.GetContacts(uint(id))
+	resp := u.Message(true, "success")
+	resp["employee"] = employee
+	u.Respond(w, resp)
+}
