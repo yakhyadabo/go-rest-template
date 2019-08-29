d5329a343776c7de8ba8657a910630bcf77b88bc
Yakhya Dabo <yakhyadabo@gmail.com>
2019-08-29 08:55:52 +0100

    [main] Remove dead code

--- go-rest-template.go
+++ go-rest-template.go
@@ -1,28 +1,28 @@
 package main
 
 import (
-	"github.com/gorilla/mux"
-	"github.com/yakhyadabo/go-rest-template/src/controller"
-	"os"
 	"log"
 	"net/http"
+
+	"github.com/gorilla/mux"
+
+	"github.com/yakhyadabo/go-rest-template/src/controller"
 )
 
 func main() {
 	router := mux.NewRouter()
 
 	router.HandleFunc("/api/user/new", controller.CreateUser).Methods("POST")
-	router.HandleFunc("/api/user", controller.GetUser).Queries("login", "{login}",).Methods("GET")
+	router.HandleFunc("/api/user", controller.GetUser).Queries("login", "{login}").Methods("GET")
 
+	router.HandleFunc("/api/employee/new", controller.CreateEmployee).Methods("POST")
+	router.HandleFunc("/api/employee", controller.GetEmployee).Queries("login", "{login}").Methods("GET")
 
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = "8000" 
-	}
+	port := "8000" //localhost
 
 	log.Println(port)
 
-	err := http.ListenAndServe(":" + port, router) 
+	err := http.ListenAndServe(":"+port, router) //Launch the app, visit localhost:8000/api
 	if err != nil {
 		log.Print(err)
 	}
